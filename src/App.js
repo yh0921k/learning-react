dc31d2147246207edc79e5386434e552bffad44b
Yonghwi Kim <yh0921k@gmail.com>
2021-06-07 15:00:04 +0900

    [add] 주석

--- src/App.js
+++ src/App.js
@@ -3,9 +3,16 @@ function App() {
   const name = "React";
   return (
     <>
-      <div className="react">{name}</div>
-      <input />
-    </>
+			{/* 주석은 이렇게 표기 */}
+			<div
+				className="react" // 시작 태그를 여러 줄로 작성한다면 이렇게 가능
+			>
+				{name}
+			</div>
+			// 하지만 이런 주석이나
+			/* 이런 주석은 페이지에 그대로 노출된다. */
+			<input />
+		</>
   );
 }
 
