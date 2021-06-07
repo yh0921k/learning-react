144f021b5e0ddfc4c9aca778ba30fad317878ec9
Yonghwi Kim <yh0921k@gmail.com>
2021-06-07 15:15:26 +0900

    [add] ESLint와 Prettier 적용하기

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import './App.css'
+import './App.css';
 function App() {
-  const name = "React";
+  const name = 'React';
   return (
     <>
-			{/* 주석은 이렇게 표기 */}
-			<div
-				className="react" // 시작 태그를 여러 줄로 작성한다면 이렇게 가능
-			>
-				{name}
-			</div>
-			// 하지만 이런 주석이나
-			/* 이런 주석은 페이지에 그대로 노출된다. */
-			<input />
-		</>
+      <h1>들여쓰기가 이상한</h1>
+      <h2>코드</h2>
+      <p>입니다.</p>
+    </>
   );
 }
 
