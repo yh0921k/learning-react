9e90254f700815881a87bbe10737b6e402578a88
Yonghwi Kim <yh0921k@gmail.com>
2021-06-07 16:23:12 +0900

    [add] 3.1 클래스형 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import './App.css';
-function App() {
-  const name = 'React';
-  return (
-    <>
-      <h1>들여쓰기가 이상한</h1>
-      <h2>코드</h2>
-      <p>입니다.</p>
-    </>
-  );
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const name = 'react';
+    return <>{name}</>;
+  }
 }
 
 export default App;
