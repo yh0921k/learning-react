b5fe9108742680fe413eb8a9af8152ebb9280da8
Yonghwi Kim <yh0921k@gmail.com>
2021-06-07 17:16:06 +0900

    [add] 3.3.7 클래스형 컴포넌트에서 props 사용하기

--- src/functional-component/MyComponent.js
+++ src/functional-component/MyComponent.js
@@ -1,22 +1,26 @@
 import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 
-const MyComponent = ({ name, children, favoriteNumber }) => {
-  return (
-    <div>
-      Hello {name} <br />
-      Hello {children} <br />
-      My Favorite Number is {favoriteNumber}
-    </div>
-  );
-};
+class MyComponent extends Component {
+  static defaultProps = {
+    name: 'JAVA',
+  };
 
-MyComponent.defaultProps = {
-  name: 'JAVA',
-};
+  static propTypes = {
+    name: PropTypes.string,
+    favoriteNumber: PropTypes.number.isRequired,
+  };
 
-MyComponent.propTypes = {
-  name: PropTypes.string,
-  favoriteNumber: PropTypes.number.isRequired,
-};
+  render() {
+    const { name, favoriteNumber, children } = this.props;
+    return (
+      <div>
+        Hello, My name is {name} <br />
+        Hello, My name is {children} <br />
+        My favorite number is {favoriteNumber}
+      </div>
+    );
+  }
+}
 
 export default MyComponent;
