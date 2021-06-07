c9c2589767b235f98d483b6dee1212e36acf3283
Yonghwi Kim <yh0921k@gmail.com>
2021-06-07 14:17:12 +0900

    [add] 2.4.1 감싸인 요소

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import { Fragment } from 'react'
+// <div> ~ </div>
+// <Fragment> ~ </Fragment>
+// <> ~ </>
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <> 
+      <h1>Hello React!</h1>
+      <h2>Hello JavaScript!</h2>
+    </>
   );
 }
 
