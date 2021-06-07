2e60c45981e977dcd17f83b3136ec70264b82d14
Yonghwi Kim <yh0921k@gmail.com>
2021-06-08 08:56:41 +0900

    [add] 3.4.1.3 this.setState 객체 대신 함수 인자 전달하기

--- src/class-component/Counter.js
+++ src/class-component/Counter.js
@@ -11,7 +11,23 @@ class Counter extends Component {
       <div>
         <h1>{number}</h1>
         <h2>{fixedNumber}</h2>
-        <button onClick={() => this.setState({ number: number + 1 })}>
+        <button
+          // onClick={() => {
+          //   this.setState({ number: number + 1 });
+          //   this.setState({ number: this.state.number + 1 });
+          // }}
+          onClick={() => {
+            this.setState((prevState) => {
+              return {
+                number: prevState.number + 1,
+              };
+            });
+
+            this.setState((prevState) => ({
+              number: prevState.number + 1,
+            }));
+          }}
+        >
           +1
         </button>
       </div>
