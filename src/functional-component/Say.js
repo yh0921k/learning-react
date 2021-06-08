79947d4b20798b5bf588f6499e61ae7174bd728f
Yonghwi Kim <yh0921k@gmail.com>
2021-06-08 09:32:41 +0900

    [add] 3.4.2.3 한 컴포넌트에서 useState 여러 번 사용하기

--- src/functional-component/Say.js
+++ src/functional-component/Say.js
@@ -5,11 +5,21 @@ const Say = () => {
   const onClickEnter = () => setMessage('Hello');
   const onClickLeave = () => setMessage('Bye');
 
+  const [color, setColor] = useState('black');
   return (
     <div>
       <button onClick={onClickEnter}>Enter</button>
       <button onClick={onClickLeave}>Leave</button>
-      <h1>{message}</h1>
+      <h1 style={{ color }}>{message}</h1>
+      <button style={{ color: 'red' }} onClick={() => setColor('red')}>
+        Red
+      </button>
+      <button style={{ color: 'green' }} onClick={() => setColor('green')}>
+        Green
+      </button>
+      <button style={{ color: 'blue' }} onClick={() => setColor('blue')}>
+        Blue
+      </button>
     </div>
   );
 };
