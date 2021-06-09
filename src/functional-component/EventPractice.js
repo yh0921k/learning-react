dc51997bbe062be7a7ff7825d646bb4e3bd82d93
Yonghwi Kim <yh0921k@gmail.com>
2021-06-09 14:13:15 +0900

    [add] 4.3.2 함수형 컴포넌트로 변환 - 객체를 사용한 useState

--- src/functional-component/EventPractice.js
+++ src/functional-component/EventPractice.js
@@ -1,22 +1,29 @@
 import React, { useState } from 'react';
 
 const EventPractice = () => {
-  const [username, setUsername] = useState('');
-  const [message, setMessage] = useState('');
-  const onChangeUsername = (e) => setUsername(e.target.value);
-  const onChangeMessage = (e) => setMessage(e.target.value);
+  const [form, setForm] = useState({ username: '', message: '' });
+  const { username, message } = form;
 
+  const onChange = (e) => {
+    const nextForm = {
+      ...form, // 기존의 form 내용을 여기에 복사한 뒤
+      [e.target.name]: e.target.value, // 원하는 값을 덮어 씌우기
+    };
+    setForm(nextForm);
+  };
   const onClick = () => {
     alert(username + ': ' + message);
-    setUsername('');
-    setMessage('');
+    setForm({
+      username: '',
+      message: '',
+    });
   };
-
   const onKeyPress = (e) => {
     if (e.key === 'Enter') {
       onClick();
     }
   };
+
   return (
     <div>
       <h1>Practice</h1>
@@ -24,13 +31,13 @@ const EventPractice = () => {
         name={'username'}
         placeholder={'User Name'}
         value={username}
-        onChange={onChangeUsername}
+        onChange={onChange}
       />
       <input
         name={'message'}
         placeholder={'Input Anything'}
         value={message}
-        onChange={onChangeMessage}
+        onChange={onChange}
         onKeyPress={onKeyPress}
       />
       <button onClick={onClick}>확인</button>
