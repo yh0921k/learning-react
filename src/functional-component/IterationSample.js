2c9cea58dc168c633bd53ee3f5e953ccd741c334
Yonghwi Kim <yh0921k@gmail.com>
2021-06-10 11:29:14 +0900

    [add] 6.3 key

--- src/functional-component/IterationSample.js
+++ src/functional-component/IterationSample.js
@@ -2,8 +2,7 @@ import React from 'react';
 
 const IterationSample = () => {
   const names = ['A1', 'A2', 'A3', 'A4'];
-  const nameList = names.map((name) => <li>{name}</li>);
-
+  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
   return (
     <div>
       <ul>{nameList}</ul>
