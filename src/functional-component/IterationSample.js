84901ea0ea52956c90f9445b5a74383ae383933e
Yonghwi Kim <yh0921k@gmail.com>
2021-06-10 11:37:49 +0900

    [add] 6.4.1 초기 상태 지정하기

--- src/functional-component/IterationSample.js
+++ src/functional-component/IterationSample.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const IterationSample = () => {
-  const names = ['A1', 'A2', 'A3', 'A4'];
-  const nameList = names.map((name, index) => <li key={index}>{name}</li>);
+  const [names, setNames] = useState([
+    { id: 1, text: 'A1' },
+    { id: 2, text: 'A2' },
+    { id: 3, text: 'A3' },
+    { id: 4, text: 'A4' },
+  ]);
+
+  const [inputText, setInputText] = useState('');
+  const [nextId, setNextId] = useState(5); // 새로운 항목을 추가할 때 사용할 id
+
+  const nameList = names.map((name) => <li key={name.id}>{name.text}</li>);
   return (
     <div>
       <ul>{nameList}</ul>
