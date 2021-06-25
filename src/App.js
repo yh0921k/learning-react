18861d93b9d5180b5e7fcf1992a78ed4db3ef24d
Yonghwi Kim <yh0921k@gmail.com>
2021-06-25 15:38:01 +0900

    [add] 7.3.1 ~ 7.3.2
    7.3.1 예제 컴포넌트 생성
    7.3.2 App 컴포넌트에서 예제 컴포넌트 사용

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React, { Component } from 'react';
-import IterationSample from './functional-component/IterationSample';
+import LifeCycleSample from './class-component/LifeCycleSample';
 
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 class App extends Component {
+  state = {
+    color: '#000000',
+  };
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor(),
+    });
+  };
+
   render() {
     return (
       <div>
-        <IterationSample />
+        <button onClick={this.handleClick}>Random Color</button>
+        <LifeCycleSample color={this.state.color} />
       </div>
     );
   }
