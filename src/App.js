77016f29302d7edc923488892f519c67fe520622
Yonghwi Kim <yh0921k@gmail.com>
2021-06-29 16:06:56 +0900

    [add] 8.3.2 useReducer - 카운터 구현하기

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import React, { useState } from 'react';
-import Info from './functional-component/Info';
+import React from 'react';
+import Counter from './functional-component/Counter';
 
 const App = () => {
-  const [visible, setVisible] = useState(false);
   return (
     <div>
-      <button onClick={() => setVisible(!visible)}>
-        {visible ? '숨기기' : '보이기'}
-      </button>
-      <hr />
-      {visible && <Info />}
+      <Counter />
     </div>
   );
 };
--- src/functional-component/Counter.js
+++ src/functional-component/Counter.js
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
+function reducer(state, action) {
+  // action.type에 따라 다른 작업 수행
+  switch (action.type) {
+    case 'INCREMENT':
+      return { value: state.value + 1 };
+    case 'DECREMENT':
+      return { value: state.value - 1 };
+    default:
+      return state;
+  }
+}
 const Counter = () => {
-  const [value, setValue] = useState(0);
+  const [state, dispatch] = useReducer(reducer, { value: 0 });
+
   return (
     <div>
-      <p>
-        Current Value : <b>{value}</b>
-      </p>
-      <button onClick={() => setValue(value + 1)}>+1</button>
-      <button onClick={() => setValue(value - 1)}>-1</button>
+      <p>Current Counter Value : {state.value}</p>
+      <button onClick={() => dispatch({ type: 'INCREMENT' })}>+</button>
+      <button onClick={() => dispatch({ type: 'DECREMENT' })}>-</button>
     </div>
   );
 };
