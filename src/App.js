d4eb15b358ab111328eecd25fef5a56b4163293e
Yonghwi Kim <yh0921k@gmail.com>
2021-06-29 13:58:24 +0900

    [add] 8.1.0 useState 기본

--- src/App.js
+++ src/App.js
@@ -1,31 +1,12 @@
-import React, { Component } from 'react';
-import LifeCycleSample from './class-component/LifeCycleSample';
-import ErrorBoundary from './class-component/ErrorBoundary';
+import React from 'react';
+import Counter from './functional-component/Counter';
 
-function getRandomColor() {
-  return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
-class App extends Component {
-  state = {
-    color: '#000000',
-  };
-
-  handleClick = () => {
-    this.setState({
-      color: getRandomColor(),
-    });
-  };
-
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleClick}>Random Color</button>
-        <ErrorBoundary>
-          <LifeCycleSample color={this.state.color} />
-        </ErrorBoundary>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Counter />
+    </div>
+  );
+};
 
 export default App;
