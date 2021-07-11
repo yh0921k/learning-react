2d33a76c4471695c95c19ffea88db5e984db6fb0
Yonghwi Kim <yh0921k@gmail.com>
2021-07-11 23:42:13 +0900

    [add] 9.3.1 classnames

--- src/css/CSSModule.js
+++ src/css/CSSModule.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import styles from './CSSModule.module.css';
+
+// 미리 styles에서 클래스를 받아 오도록 설정
+const cx = classNames.bind(styles);
 
 const CssModule = () => {
   return (
-    <div className={`${styles.wrapper} ${styles.inverted}`}>
+    <div className={cx('wrapper', 'inverted')}>
       Hello, I'm <span className="something">CSS Module!</span>
     </div>
   );
