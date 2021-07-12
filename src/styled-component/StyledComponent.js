903c66d19bf23c314a1144bc25caccd3de594db6
Yonghwi Kim <yh0921k@gmail.com>
2021-07-12 17:01:38 +0900

    [add] 9.4.5.1 styled-component : 반응형 디자인 모듈화

--- src/styled-component/StyledComponent.js
+++ src/styled-component/StyledComponent.js
@@ -1,24 +1,31 @@
 import styled, { css } from 'styled-components';
 import React from 'react';
 
+const sizes = {
+  desktop: 1024,
+  tablet: 768,
+};
+
+// 위에 있는 size 객체에 따라 자동으로 media 쿼리 함수를 만들어 준다.
+// 참고 : https://www.styled-components.com/docs/advanced#media-templates
+const media = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] / 16}em) {
+      ${css(...args)};
+    }
+  `;
+  return acc;
+}, {});
+
 const Box = styled.div`
   /* props로 넣어준 값을 직접 전달해 줄 수 있다. */
   background: ${(props) => props.color || 'blue'};
   padding: 1rem;
   display: flex;
-  /* 
-  기본적으로 가로 크기 1024px에 가운데 정렬을 하고
-  가로 크기가 작아짐에 따라 크기를 줄이고, 
-  768px 미만이 되면 가득 채운다.
-  */
   width: 1024px;
   margin: 0 auto;
-  @media (max-width: 1024px) {
-    width: 768px;
-  }
-  @media (max-width: 768px) {
-    width: 100%;
-  }
+  ${media.desktop`width: 768px;`}
+  ${media.tablet`width: 100%;`}
 `;
 
 const Button = styled.button`
