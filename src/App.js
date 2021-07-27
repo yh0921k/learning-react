90d159a1adc833d0fc94f96f1bcd129ec50f8956
Yonghwi Kim <yh0921k@gmail.com>
2021-07-27 15:02:11 +0900

    [add] 14.4.2 NewsList 만들기

--- src/App.js
+++ src/App.js
@@ -1,33 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './news-viewer/NewsList';
 
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=7eff071e480f495bad5864b7ce9b506e',
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && (
-        <textarea
-          rows={7}
-          value={JSON.stringify(data, null, 2)}
-          readOnly={true}
-        />
-      )}
-    </div>
-  );
+  return <NewsList />;
 };
 
 export default App;
