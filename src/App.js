2131a923b0260af66a4251a3d104262b4693338f
Yonghwi Kim <yh0921k@gmail.com>
2021-07-27 15:49:09 +0900

    [add] 14.6.1 카테고리 선택 UI 만들기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import NewsList from './news-viewer/NewsList';
+import Categories from './news-viewer/Categories';
 
 const App = () => {
-  return <NewsList />;
+  const [category, setCategory] = useState('all');
+  const onSelect = useCallback((category) => setCategory(category), []);
+
+  return (
+    <>
+      <Categories category={category} onSelect={onSelect} />
+      <NewsList category={category} />
+    </>
+  );
 };
 
 export default App;
