90e3b8a9636a927349833f38aa04f9c4d7d037cb
Yonghwi Kim <yh0921k@gmail.com>
2021-07-27 10:32:48 +0900

    [add] 13.2.3 페이지 만들기

---
+++ src/react-router/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>This project is designed to learn react router basic</p>
+    </div>
+  );
+};
+
+export default About;
