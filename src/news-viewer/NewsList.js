cf76366de360ffb28e83532d19c4268d252f03df
Yonghwi Kim <yh0921k@gmail.com>
2021-07-27 16:53:47 +0900

    [add] 14.8 usePromise 커스텀 Hook 만들기

--- src/news-viewer/NewsList.js
+++ src/news-viewer/NewsList.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
 import axios from 'axios';
+import usePromise from './usePromise';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -17,25 +18,11 @@ const NewsListBlock = styled.div`
 `;
 
 const NewsList = ({ category }) => {
-  const [articles, setArticles] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    // async를 사용하는 함수 따로 선언
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const query = category === 'all' ? '' : `&category=${category}`;
-        const response = await axios.get(
-          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=7eff071e480f495bad5864b7ce9b506e`,
-        );
-        setArticles(response.data.articles);
-      } catch (e) {
-        console.log(e);
-      }
-      setLoading(false);
-    };
-    fetchData();
+  const [loading, response, error] = usePromise(() => {
+    const query = category === 'all' ? '' : `&category=${category}`;
+    return axios.get(
+      `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=7eff071e480f495bad5864b7ce9b506e`,
+    );
   }, [category]);
 
   // 대기중
@@ -43,12 +30,19 @@ const NewsList = ({ category }) => {
     return <NewsListBlock>Wait ...</NewsListBlock>;
   }
 
-  // 아직 articles 값이 설정되지 않았을 경우
-  if (!articles) {
+  // 아직 response 값이 설정되지 않았을 경우
+  if (!response) {
     return null;
   }
 
-  // articles 값이 유효할 때
+  // 에러가 발생했을 때
+  if (error) {
+    return <NewsListBlock>에러 발생</NewsListBlock>;
+  }
+
+  // response 값이 유효할 때
+  const { articles } = response.data;
+
   return (
     <NewsListBlock>
       {articles.map((article) => (
