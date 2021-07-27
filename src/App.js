1d945257d092a6593c90cd818316f2fa61cee80e
Yonghwi Kim <yh0921k@gmail.com>
2021-07-27 11:57:20 +0900

    [add] 13.6.1 history

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Home from './react-router/Home';
 import { Link, Route } from 'react-router-dom';
 import About from './react-router/About';
 import Profiles from './react-router/Profiles';
+import HistorySample from './react-router/HistorySample';
 
 const App = () => {
   return (
@@ -17,11 +18,15 @@ const App = () => {
         <li>
           <Link to={'/profiles'}>Profile</Link>
         </li>
+        <li>
+          <Link to={'/history'}>History</Link>
+        </li>
       </ul>
       <hr />
       <Route path={'/'} component={Home} exact={true} />
       <Route path={['/about', '/info']} component={About} />
       <Route path={'/profiles'} component={Profiles} />
+      <Route path={'/history'} component={HistorySample} />
     </div>
   );
 };
