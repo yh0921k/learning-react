3a864f087270b0fc0d1c555f7a5ad1b48680c393
Yonghwi Kim <yh0921k@gmail.com>
2021-07-28 14:26:13 +0900

    [add] 17.4.1 스토어 만들기

--- src/index.js
+++ src/index.js
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import rootReducer from './react-redux/modules';
+import { createStore } from 'redux';
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById('root'),
-);
+const store = createStore(rootReducer);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
