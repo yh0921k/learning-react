5835280afacb45046f8b52cf0ebf3674b27d9410
Yonghwi Kim <yh0921k@gmail.com>
2021-07-28 14:07:38 +0900

    [add] 17.3.2.2 액션 생성 함수 만들기

--- src/react-redux/modules/todos.js
+++ src/react-redux/modules/todos.js
@@ -2,3 +2,28 @@ const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경
 const INSERT = 'todos/INSERT'; // 새로운 항목 등록
 const TOGGLE = 'todos/TOGGLE'; // 항목 체크 상태를 변경
 const REMOVE = 'todos/REMOVE'; // 항목을 제거
+
+export const changeInput = (input) => ({
+  type: CHANGE_INPUT,
+  input,
+});
+
+let id = 3; // insert가 호출될 때마다 1씩 더해짐
+export const insert = (text) => ({
+  type: INSERT,
+  todo: {
+    id: id++,
+    text,
+    done: false,
+  },
+});
+
+export const toggle = (id) => ({
+  type: TOGGLE,
+  id,
+});
+
+export const remove = (id) => ({
+  type: REMOVE,
+  id,
+});
