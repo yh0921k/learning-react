81d845275fc8651b92b291431d263c362e36904e
Yonghwi Kim <yh0921k@gmail.com>
2021-07-28 14:01:05 +0900

    [add] 17.3.2.1 액션 타입 정의하기

---
+++ src/react-redux/modules/todos.js
@@ -0,0 +1,4 @@
+const CHANGE_INPUT = 'todos/CHANGE_INPUT'; // 인풋 값을 변경
+const INSERT = 'todos/INSERT'; // 새로운 항목 등록
+const TOGGLE = 'todos/TOGGLE'; // 항목 체크 상태를 변경
+const REMOVE = 'todos/REMOVE'; // 항목을 제거
