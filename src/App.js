a7ce5ec7ace3a09a91d0131000165d5940216f3d
Yonghwi Kim <yh0921k@gmail.com>
2021-07-28 13:22:22 +0900

    [add] 17.2.2 할 일 목록 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import Counter from './react-redux/components/Counter';
+import Todos from './react-redux/components/Todos';
 
 const App = () => {
   return (
     <div>
       <Counter number={0} />
+      <hr />
+      <Todos />
     </div>
   );
 };
