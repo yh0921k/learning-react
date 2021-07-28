36f2079271528e5a8557506f0a9add60ab604f43
Yonghwi Kim <yh0921k@gmail.com>
2021-07-28 10:02:15 +0900

    [add] 15.3.2 새로워진 Context를 프로젝트에 반영하기

--- src/contexts/ColorBox.js
+++ src/contexts/ColorBox.js
@@ -1,15 +1,28 @@
 import React from 'react';
-import ColorContext from './color';
+import { ColorConsumer } from './color';
 
 const ColorBox = () => {
   return (
-    <ColorContext.Consumer>
-      {(value) => (
-        <div
-          style={{ width: '64px', height: '64px', background: value.color }}
-        />
+    <ColorConsumer>
+      {({ state }) => (
+        <>
+          <div
+            style={{
+              width: '64px',
+              height: '64px',
+              background: state.color,
+            }}
+          />
+          <div
+            style={{
+              width: '32px',
+              height: '32px',
+              background: state.subColor,
+            }}
+          />
+        </>
       )}
-    </ColorContext.Consumer>
+    </ColorConsumer>
   );
 };
 
