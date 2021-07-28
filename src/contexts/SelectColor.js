3667a612619d2632282a80e8facd31b7cc66f236
Yonghwi Kim <yh0921k@gmail.com>
2021-07-28 10:29:43 +0900

    [add] 15.4.2 static contextType 사용하기

--- src/contexts/SelectColor.js
+++ src/contexts/SelectColor.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import { ColorConsumer } from './color';
+import React, { Component } from 'react';
+import ColorContext, { ColorConsumer } from './color';
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
 
-const SelectColor = () => {
-  return (
-    <div>
-      <h2>색상 선택</h2>
-      <ColorConsumer>
-        {({ actions }) => (
-          <div style={{ display: 'flex' }}>
-            {colors.map((color) => (
-              <div
-                key={color}
-                style={{
-                  background: color,
-                  width: '24px',
-                  height: '24px',
-                  cursor: 'pointer',
-                }}
-                onClick={() => actions.setColor(color)}
-                onContextMenu={(e) => {
-                  e.preventDefault(); // 마우스 오른쪽 버튼 클릭 시 메뉴가 뜨는 것을 무시함
-                  actions.setSubColor(color);
-                }}
-              />
-            ))}
-          </div>
-        )}
-      </ColorConsumer>
-      <hr />
-    </div>
-  );
-};
+class SelectColor extends Component {
+  static contextType = ColorContext;
+
+  handleSetColor = (color) => {
+    this.context.actions.setColor(color);
+  };
+
+  handleSetSubColor = (subColor) => {
+    this.context.actions.setSubColor(subColor);
+  };
+
+  render() {
+    return (
+      <div>
+        <h2>색상 선택</h2>
+        <div style={{ display: 'flex' }}>
+          {colors.map((color) => (
+            <div
+              key={color}
+              style={{
+                background: color,
+                width: '24px',
+                height: '24px',
+                cursor: 'pointer',
+              }}
+              onClick={() => this.handleSetColor(color)}
+              onContextMenu={(e) => {
+                e.preventDefault();
+                this.handleSetSubColor(color);
+              }}
+            />
+          ))}
+        </div>
+        <hr />
+      </div>
+    );
+  }
+}
 
 export default SelectColor;
