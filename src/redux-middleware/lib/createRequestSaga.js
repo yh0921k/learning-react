0e8bf29eff84296c02020b6f41543b9e4aca8990
Yonghwi Kim <yh0921k@gmail.com>
2021-07-29 14:56:08 +0900

    [add] 18.3.2.4 리팩토링

---
+++ src/redux-middleware/lib/createRequestSaga.js
@@ -0,0 +1,25 @@
+import { finishLoading, startLoading } from '../modules/loading';
+import { put, call } from 'redux-saga/effects';
+
+export default function createRequestSaga(type, request) {
+  const SUCCESS = `${type}_SUCCESS`;
+  const FAILURE = `${type}_FAILURE`;
+
+  return function* (action) {
+    yield put(startLoading(type)); // 로딩 시작
+    try {
+      const response = yield call(request, action.payload);
+      yield put({
+        type: SUCCESS,
+        payload: response.data,
+      });
+    } catch (e) {
+      yield put({
+        type: FAILURE,
+        payload: e,
+        error: true,
+      });
+    }
+    yield put(finishLoading(type)); // 로딩 끝
+  };
+}
--- src/redux-middleware/modules/sample.js
+++ src/redux-middleware/modules/sample.js
@@ -1,7 +1,7 @@
 import * as api from '../lib/api';
 import { createAction, handleActions } from 'redux-actions';
-import { call, put, takeLatest } from 'redux-saga/effects';
-import { finishLoading, startLoading } from './loading';
+import { takeLatest } from 'redux-saga/effects';
+import createRequestSaga from '../lib/createRequestSaga';
 
 const GET_POST = 'sample/GET_POST';
 const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
@@ -17,45 +17,8 @@ const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
 export const getPost = createAction(GET_POST, (id) => id);
 export const getUsers = createAction(GET_USERS, api.getUsers);
 
-function* getPostSaga(action) {
-  yield put(startLoading(GET_POST)); // 로딩 시작
-  // 파라미터로 action을 받아 오면 액션의 정보를 조회할 수 있다.
-  try {
-    // call을 사용하면 Promise를 반환하는 함수를 호출하고 기다릴 수 있다.
-    // 첫 번째 파라미터는 함수, 나머지 파라미터는 해당 함수에 넣을 인수이다.
-    const post = yield call(api.getPost, action.payload); // api.getPost(action.payload)를 의미
-    yield put({
-      type: GET_POST_SUCCESS,
-      payload: post.data,
-    });
-  } catch (e) {
-    // try ~ catch 문을 사용하여 에러도 잡을 수 있다.
-    yield put({
-      type: GET_POST_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_POST)); // 로딩 완료
-}
-
-function* getUsersSaga() {
-  yield put(startLoading(GET_USERS));
-  try {
-    const users = yield call(api.getUsers);
-    yield put({
-      type: GET_USERS_SUCCESS,
-      payload: users.data,
-    });
-  } catch (e) {
-    yield put({
-      type: GET_USERS_FAILURE,
-      payload: e,
-      error: true,
-    });
-  }
-  yield put(finishLoading(GET_USERS));
-}
+export const getPostSaga = createRequestSaga(GET_POST, api.getPost);
+export const getUsersSaga = createRequestSaga(GET_USERS, api.getUsers);
 
 export function* sampleSaga() {
   yield takeLatest(GET_POST, getPostSaga);
