22bcc3466168dcef73c24fe575aed14794e5515c
Yonghwi Kim <yh0921k@gmail.com>
2021-07-29 16:47:22 +0900

    [add] 19.1 자바스크립트 함수 비동기 로딩

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import CounterContainer from './redux-middleware/containers/CounterContainer';
 
 const App = () => {
+  const onClick = () => {
+    import('./code-splitting/notify').then((result) => result.default());
+  };
+
   return (
-    <div>
-      <CounterContainer />
+    <div className={'App'}>
+      <header className={'App-header'}>
+        <p onClick={onClick}>Hello React</p>
+      </header>
     </div>
   );
 };
