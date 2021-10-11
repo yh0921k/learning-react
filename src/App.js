70bf6e153c839c810ece86d1a47669f586c9e422
Yonghwi Kim <yh0921k@gmail.com>
2021-10-11 12:40:00 +0900

    add: 1.1.5.2 Custom Element
    - props를 사용한 Custom Element 작성
    - props임을 children으로 확인
    - children에는 제한이 없음

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 const App = () => {
-  const paint = (title, description) => (
+  const Paint = ({ title, description, children }) => (
     <>
       <h1>{title}</h1>
       <h3>{description}</h3>
+      {children}
     </>
   );
   return (
     <div>
-      {paint('Good', 'good')}
-      {paint('Bad', 'bad')}
+      <Paint title={'Good'} description={'good'}>
+        <h1>Children</h1>
+      </Paint>
+      <Paint title={'Bad'} description={'bad'}>
+        Hello React
+      </Paint>
     </div>
   );
 };
