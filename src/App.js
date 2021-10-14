312ecd3157292dda0d87a457284ce467ebf2641f
Yonghwi Kim <yh0921k@gmail.com>
2021-10-14 20:43:05 +0900

    add: 1.1.17 Hook Flow 02

--- src/App.js
+++ src/App.js
@@ -1,5 +1,48 @@
 import React, { useEffect, useState } from 'react';
 
+const Child = () => {
+  console.log('   Child render Start');
+  const [text, setText] = useState(() => {
+    console.log('   Child useState()');
+    return '';
+  });
+
+  useEffect(() => {
+    console.log('   Child useEffect() No deps');
+
+    return () => {
+      console.log('   Child useEffect() No deps Clean Up');
+    };
+  });
+
+  useEffect(() => {
+    console.log('   Child useEffect() Empty deps ');
+
+    return () => {
+      console.log('   Child useEffect() Empty deps Clean Up');
+    };
+  }, []);
+
+  useEffect(() => {
+    console.log('   Child useEffect() [text] ');
+
+    return () => {
+      console.log('   Child useEffect() [text] Clean Up');
+    };
+  }, [text]);
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+  const element = (
+    <>
+      <input onChange={handleChange} />
+      <p>{text}</p>
+    </>
+  );
+  console.log('   Child render End');
+  return element;
+};
 const App = () => {
   console.log('App render Start');
   const [show, setShow] = useState(() => {
@@ -9,29 +52,36 @@ const App = () => {
 
   useEffect(() => {
     console.log('App useEffect() No deps');
+
+    return () => {
+      console.log('App useEffect() No deps Clean Up');
+    };
   });
 
   useEffect(() => {
     console.log('App useEffect() Empty deps ');
+
+    return () => {
+      console.log('App useEffect() Empty deps Clean Up');
+    };
   }, []);
 
   useEffect(() => {
     console.log('App useEffect() [show] ');
+
+    return () => {
+      console.log('App useEffect() [show] Clean Up');
+    };
   }, [show]);
 
   const handleClick = () => {
     setShow((prev) => !prev);
   };
+  console.log('App render End');
   return (
     <>
       <button onClick={handleClick}>Search</button>
-      {show ? (
-        <>
-          <input />
-          <p></p>
-        </>
-      ) : null}
-      {console.log('App render End')}
+      {show ? <Child /> : null}
     </>
   );
 };
