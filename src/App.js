5132535646acbdedffbd48ecc9f0c832b9d4979b
Yonghwi Kim <yh0921k@gmail.com>
2021-10-18 15:36:01 +0900

    add: 1.2.4.2 Extraction

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Composition from './fastcampus/components/2-4Props/Composition';
+import Extraction from './fastcampus/components/2-4Props/Extraction';
 
 const App = () => {
   return (
     <>
+      <Extraction />
       <Composition />
     </>
   );
